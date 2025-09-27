523e8bc6e0574c16ac07e4b61d1d0ec335cbdc9c
NameeNomen <fat.lp3i33@gmail.com>
2025-09-27 12:02:59 +0700

    masih pusing dengan proses

--- lib/latihan/icon.dart
+++ lib/latihan/icon.dart
@@ -24,7 +24,100 @@ class BelajarIcon extends StatelessWidget {
               )
             ],
           ),
+           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+              
+              Text("Training", style: TextStyle(
+                color: const Color(0xffFF69B4),
+                fontSize: 20,
+              ),),
+                ],
+              ),
+                SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+              Text("Your Program", style: TextStyle(
+                color: const Color(0xffFF69B4), 
+                fontSize: 20,
+              ),),
+              Text("Detail", style: TextStyle(
+                color: const Color(0xffFF69B4), 
+                fontSize: 20,
+              ),),
+            ],
+            ),
+          Container(
+                padding: EdgeInsets.all(20),
+                width: 400,
+                height: 210,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0xffffe4ec), Color(0xffe1bee7)], // pink muda → ungu muda
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                
+                  ),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(80),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)
+                  )
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                     Text("Next Workout", style: TextStyle(
+                  color: const Color(0xffFF69B4), 
+                  fontSize: 10,
+                ),
+                ),
+                 Text("Lets Toning", style: TextStyle(
+                  color: const Color(0xffFF69B4), 
+                  fontSize: 20,
+                ),
+                ),
+                 Text("ang Glutes Workout", style: TextStyle(
+                  color: const Color(0xffFF69B4), 
+                  fontSize: 20,
+                ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                    children: [
+                      Icon(Icons.alarm),
+                       Text("60 min", style: TextStyle(
+                  color: const Color(0xffFF69B4), 
+                  fontSize: 10,
+                ),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    
+                  ),
+                )
+                    ],
+                )
+                  ],
+                ),
+
+          )
+
+            ],
         ),
+      ),
       ),
       );
 
